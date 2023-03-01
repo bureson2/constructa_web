f27fd51c19ef0330f335c14be2a7143b900c418f
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2023-03-01 18:21:06 +0100

    FE init with test tables for users and tasks

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import UserList from "./components/UserList";
+import TaskList from "./components/TaskList";
+
+function App() {
+    return (
+        <>
+          <UserList></UserList>
+          <TaskList></TaskList>
+        </>
+)
+    ;
+}
+
+export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+const TaskList = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/tasks')
+            .then(response => {
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+    return (
+        <div>
+            <h2>Seznam úkolů</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Úkol</th>
+                    <th>Popis úkolu</th>
+                    <th>Místo</th>
+                    <th>Řešitel</th>
+                    <th>Od:</th>
+                    <th>Do:</th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map(user => (
+                    <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.description}</td>
+                        <td>{user.id}</td>
+                        <td>{user.locationTime}</td>
+                        <td>{user.assignee ? user.assignee.firstname.concat(" ", user.assignee.lastname) : "-"}</td>
+                        <td>{user.timeFrom}</td>
+                        <td>{user.timeTo}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default TaskList;
