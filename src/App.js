5375d0d3fe884570adb10e69437a2c1d327f96b4
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2023-03-09 23:43:10 +0100

    test login with authorization

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import UserList from "./components/UserList";
 import TaskList from "./components/TaskList";
+import LoginForm from "./pages/LoginForm";
 
 function App() {
     return (
         <>
-          <UserList></UserList>
-          <TaskList></TaskList>
+          {/*<UserList></UserList>*/}
+          {/*<TaskList></TaskList>*/}
+            <LoginForm></LoginForm>
         </>
 )
     ;
